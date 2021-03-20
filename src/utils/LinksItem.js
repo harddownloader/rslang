fe58dca2664ed0d822ae1a899adeb798e08227cd
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-20 16:52:56 +0200

    add LinksItem.js

---
+++ src/utils/LinksItem.js
@@ -0,0 +1,17 @@
+export default [
+	{
+		path: '',
+		bg: '#363538',
+		title: '#363538',
+	},
+	{
+		path: '',
+		bg: '#303032',
+		title: '',
+	},
+	{
+		path: '',
+		bg: '#28282a',
+		title: '',
+	},
+]
