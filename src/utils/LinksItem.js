301323b212a3f16baf45c46c43e3afdbe4b72bd6
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-21 19:02:33 +0200

    feat: add img/ subtitle to linkObg

--- src/utils/LinksItem.js
+++ src/utils/LinksItem.js
@@ -1,22 +1,35 @@
+import gameIco from '@/assets/images/gameImg.jpg'
+import mainIco from '@/assets/images/mainIco.jpg'
+import statIco from '@/assets/images/statIco.jpg'
+import vocIco from '@/assets/images/vocImage.jpg'
+
 export default [
 	{
-		path: '',
+		path: '/',
 		bg: '#363538',
-		title: '#363538',
+		title: 'Main',
+		subtitle: 'Главная страница',
+		img: mainIco,
 	},
 	{
-		path: '',
+		path: '/vocabulary',
 		bg: '#303032',
-		title: '',
+		title: 'Vocabulary',
+		subtitle: 'Cловарь',
+		img: vocIco,
 	},
 	{
-		path: '',
+		path: '/games',
 		bg: '#28282a',
-		title: '',
+		title: 'Games',
+		subtitle: 'Игры',
+		img: gameIco,
 	},
 	{
-		path: '',
+		path: '/statistics',
 		bg: '#24282a',
-		title: '',
+		title: 'Statistics',
+		subtitle: 'Статистика',
+		img: statIco,
 	},
 ]
