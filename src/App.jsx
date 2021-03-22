5e7df38c000b54bfb04c37bb64791a6f4c6b6318
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-22 13:49:49 +0200

    feat: destract App/ add simple GamesPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import { makeStyles } from '@material-ui/core/styles'
 
 import Home from '@/pages/Home'
+import Games from '@/pages/Games'
 import Statistics from '@/pages/Statistics'
 
 const useStyles = makeStyles(theme => ({
@@ -20,6 +21,29 @@ const useStyles = makeStyles(theme => ({
 	},
 }))
 
+const routers = [
+	{
+		path: '/',
+		component: <Home />,
+	},
+	{
+		path: '/games',
+		component: <Games />,
+	},
+	{
+		path: '/statistics',
+		component: <Statistics />,
+	},
+	{
+		path: '/vocabulary',
+		component: <div></div>,
+	},
+	{
+		path: '*',
+		component: <div></div>,
+	},
+]
+
 const App = () => {
 	const classes = useStyles()
 	return (
@@ -27,26 +51,11 @@ const App = () => {
 			<CssBaseline />
 			<div className={classes.root}>
 				<Switch>
-					{/* main */}
-					<Route exact path='/'>
-						<Home />
-					</Route>
-					{/* games */}
-					<Route path='/games'>
-						<Home />
-					</Route>
-					{/* stat */}
-					<Route path='/statistics'>
-						<Statistics />
-					</Route>
-					{/* book */}
-					<Route path='/vocabulary'>
-						<Home />
-					</Route>
-					{/* out */}
-					<Route path='*'>
-						<Home />
-					</Route>
+					{routers.map((rout, index) => (
+						<Route exact path={rout.path} key={index}>
+							{rout.component}
+						</Route>
+					))}
 				</Switch>
 			</div>
 		</Router>
---
+++ src/pages/Games.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from '@/components/header'
+
+const Games = () => {
+	return (
+		<>
+			<Header />
+		</>
+	)
+}
+
+export default Games
