8a1d444500da81ba8ea0ea4e7f4edb15767bf6e0
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-03-22 13:58:21 +0200

    fix: destract App/create Routers.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 // import Container from '@material-ui/core/Container'
 import { makeStyles } from '@material-ui/core/styles'
 
-import Home from '@/pages/Home'
-import Games from '@/pages/Games'
-import Statistics from '@/pages/Statistics'
+import RouterList from './RouterList'
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -21,29 +19,6 @@ const useStyles = makeStyles(theme => ({
 	},
 }))
 
-const routers = [
-	{
-		path: '/',
-		component: <Home />,
-	},
-	{
-		path: '/games',
-		component: <Games />,
-	},
-	{
-		path: '/statistics',
-		component: <Statistics />,
-	},
-	{
-		path: '/vocabulary',
-		component: <div></div>,
-	},
-	{
-		path: '*',
-		component: <div></div>,
-	},
-]
-
 const App = () => {
 	const classes = useStyles()
 	return (
@@ -51,7 +26,7 @@ const App = () => {
 			<CssBaseline />
 			<div className={classes.root}>
 				<Switch>
-					{routers.map((rout, index) => (
+					{RouterList.map((rout, index) => (
 						<Route exact path={rout.path} key={index}>
 							{rout.component}
 						</Route>
---
+++ src/RouterList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Home from '@/pages/Home'
+import Games from '@/pages/Games'
+import Statistics from '@/pages/Statistics'
+
+export default [
+	{
+		path: '/',
+		component: <Home />,
+	},
+	{
+		path: '/games',
+		component: <Games />,
+	},
+	{
+		path: '/statistics',
+		component: <Statistics />,
+	},
+	{
+		path: '/vocabulary',
+		component: <div></div>,
+	},
+	{
+		path: '*',
+		component: <div></div>,
+	},
+]
