4cd05d98553087a52aee0784bd3b7c5f5d40f504
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-04-05 18:41:15 +0300

    feat: clear index

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,4 @@ import { render } from 'react-dom'
 import App from '@/App'
 import './index.css'
 
-render(
-	<App />,
-
-	document.getElementById('root'),
-)
+render(<App />, document.getElementById('root'))
