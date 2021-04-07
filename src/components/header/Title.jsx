d0a8b9ab7e33acd6889e0c4acb1d3de0d5f340a5
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-04-07 15:31:27 +0300

    fix: title

--- src/components/header/Title.jsx
+++ src/components/header/Title.jsx
@@ -6,24 +6,26 @@ import colors from '@/utils/colors'
 
 const useStyles = makeStyles(theme => ({
 	title: {
-		// cursor: 'pointer',
-		// '&:hover': {
-		// 	color: colors.borderColor,
-		// },
-		// [theme.breakpoints.up('md')]: {
-		// 	transform: 'rotate(-90deg)',
-		// },
+		textTransform: 'uppercase',
+		cursor: 'pointer',
+		fontFamily:
+			'Montserrat,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+		fontWeight: 700,
+		fontSize: '1.65rem',
+		lineHeight: '1',
+		whiteSpace: 'nowrap',
+		'&:hover': {
+			color: colors.borderColor,
+		},
+		[theme.breakpoints.up('md')]: {
+			transform: 'rotate(-90deg)',
+		},
 	},
 }))
 
 const Title = () => {
 	const classes = useStyles()
-	return (
-		<div className="web-slogan">
-			<span className={classes.title}>RsLang</span>
-		</div>	
-		
-	)
+	return <span className={classes.title}>RsLang</span>
 }
 
 export default Title
