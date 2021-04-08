7982dd699d4391e48d747996b4732ac059c62ec1
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-04-08 23:56:12 +0300

    update wb config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,61 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
 	mode: 'production',
 	devtool: 'source-map',
+	optimization: {
+		moduleIds: 'deterministic',
+		minimize: true,
+		mangleWasmImports: true,
+		mangleExports: true,
+		sideEffects: true,
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+			},
+		},
+		minimizer: [
+			new TerserPlugin({
+				extractComments: true,
+			}),
+			new CssMinimizerPlugin(),
+		],
+	},
 	plugins: [
+		new ImageMinimizerPlugin({
+			severityError: 'warning', // Ignore errors on corrupted images
+			deleteOriginalAssets: true,
+			filename: '[path][name].webp',
+			minimizerOptions: {
+				plugins: ['imagemin-webp'],
+			},
+		}),
 		new MiniCssExtractPlugin({
 			filename: '[name].[contenthash].css',
 			chunkFilename: '[id].[contenthash].css',
 		}),
 	],
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
 })
