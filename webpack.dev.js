7412462e6f20cbb394c55d23459f9bdfd85222b4
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-04-08 23:56:12 +0300

    update wb config

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,17 +2,35 @@ const path = require('path')
 const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = merge(common, {
 	mode: 'development',
 	devtool: 'inline-source-map',
 	//	new ForkTsCheckerWebpackPlugin({ async: false }),
+	optimization: {
+		runtimeChunk: true,
+	},
 	devServer: {
 		contentBase: path.join(__dirname, 'dist'),
 		historyApiFallback: true,
 		open: true,
 		hot: true,
 	},
-	plugins: [new webpack.HotModuleReplacementPlugin()],
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new ESLintPlugin({
+			extensions: ['.js', 'jsx'],
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+			},
+		],
+	},
 })
