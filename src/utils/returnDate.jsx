e11f07df53269ff4a6d75f466a6e58a2e1b6f4ef
VlasMaskalenchik <vlasmaskalenchik@gmail.com>
2021-04-15 14:15:28 +0300

    add return date

---
+++ src/utils/returnDate.jsx
@@ -0,0 +1,14 @@
+
+const returnDAte = () => {
+
+    const dateObj = new Date();
+    const month = dateObj.getUTCMonth() + 1;
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+    const newdate = year + "-" + month + "-" + day;
+
+    return newdate;
+
+}
+
+export default returnDAte;
