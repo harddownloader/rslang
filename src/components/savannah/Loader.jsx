96c651e298540ab996e2641601220ad92c6f1230
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-04-16 14:01:07 +0300

    fix: add secondary color to loader

--- src/components/savannah/Loader.jsx
+++ src/components/savannah/Loader.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import { secondaryColor } from '@/assets/jss/materialKitReact'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -12,12 +13,21 @@ const useStyles = makeStyles(theme => ({
 		colorSecondary: '#f8ee3a',
 	},
 }))
+const innerTheme = createMuiTheme({
+	palette: {
+		secondary: {
+			main: '#f8ee3a',
+		},
+	},
+})
 
 export default function Loader() {
 	const classes = useStyles()
 	return (
-		<div className={classes.root}>
-			<LinearProgress color={secondaryColor} />
-		</div>
+		<ThemeProvider theme={innerTheme}>
+			<div className={classes.root}>
+				<LinearProgress color='secondary' />
+			</div>
+		</ThemeProvider>
 	)
 }
