493a36224147813ceea7006a84981ff61944c132
harddownloader <serafimdev@gmail.com>
2021-04-17 21:11:54 +0300

    little fix

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,6 +1,6 @@
 const initialState = {
-  userId: "605e409e5747abe9af64b684",
-  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNWU0MDllNTc0N2FiZTlhZjY0YjY4NCIsImlhdCI6MTYxODY4MDcxNSwiZXhwIjoxNjE4Njk1MTE1fQ.UvOfoXQVEOfJqyOi7jS9qrZsvykVt-4LG3aau7HSWgE"
+  userId: null,
+  token: null
 }
 
 export function getUserAuth(state = initialState, action) {
